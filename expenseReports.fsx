855c87c61891eff4c6ff0b560b8c7387ead793b7
Clément Bouillier <clement.bouillier@devcrafting.com>
2017-01-03 11:35:07 +0100

    Adjust amount excluding/including VAT when there are several VAT rates (try to infer amount excluding VAT for each VAT rate)

--- expenseReports.fsx
+++ expenseReports.fsx
@@ -5,12 +5,13 @@ type ExpenseReportLine = {
     Day: DateTime
     Description: string
     Amount: decimal
-    TVA: TVA list
+    VAT: VAT list
     ReceiptLocation: string
 }
-and TVA = {
-    Percent: decimal
+and VAT = {
+    Rate: decimal
     Amount: decimal
+    AmountExcludingTaxes: decimal
 }
 
 let parseDay (parts:array<string>) = DateTime.ParseExact(parts.[0], "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture)
@@ -21,32 +22,68 @@ let decimal' (s:string) = decimal (s.Replace("e", ""))
 
 let parseAmount (parts:array<string>) = decimal' parts.[2]
 
-let rec parseTVA' (parts:array<string>) remaining =
+let vat vatRate (vatParts: string array) amountIncludingTax nbVat =
+    match vatRate, vatParts.Length, nbVat with
+    | 0m, 1, _ ->
+        { Rate = vatRate; Amount = 0m; AmountExcludingTaxes = amountIncludingTax }
+    | _, 2, 1 ->
+        let vatAmount = decimal' vatParts.[1]
+        { 
+            Rate = vatRate; 
+            Amount = vatAmount;
+            AmountExcludingTaxes = amountIncludingTax - vatAmount
+        }
+    | _, 2, _ when vatParts.[1].Contains("+") ->
+        let vatAmounts = vatParts.[1].Split('+')
+        let vatAmount = decimal' vatAmounts.[1]
+        { 
+            Rate = vatRate; 
+            Amount = vatAmount;
+            AmountExcludingTaxes = decimal' vatAmounts.[0]
+        }
+    | _, 2, _ -> 
+        let vatAmount = decimal' vatParts.[1]
+        let amountExcludingVat = vatAmount / vatRate * 100m
+        let assumeAmountIncludingVatHasNoCentsUnit = Decimal.Round(amountExcludingVat + vatAmount, 1, MidpointRounding.AwayFromZero)
+        let diff = Math.Abs(amountExcludingVat + vatAmount - assumeAmountIncludingVatHasNoCentsUnit)
+        { 
+            Rate = vatRate; 
+            Amount = vatAmount;
+            AmountExcludingTaxes = amountExcludingVat + diff
+        }
+    | _, _, _ -> failwithf "Unknown case with VAT rate %M and amounts '%s'" vatRate (vatParts |> String.concat "=") 
+
+let rec parseVAT' (parts:array<string>) remaining totalAmount nbVat =
     if remaining > 0 then
-        let tvaParts = parts.[parts.Length - remaining].Split('=')
-        let tva =
-            match tvaParts.[0] with
-            | "TVA0" -> { Percent = 0m; Amount = 0m }
-            | "TVA5.5" -> { Percent = 5.5m; Amount = decimal' tvaParts.[1]}
-            | "TVA10" -> { Percent = 10m; Amount = decimal' tvaParts.[1]}
-            | "TVA20" -> { Percent = 20m; Amount = decimal' tvaParts.[1]}
-            | unknown -> failwithf "Unknown TVA %s" unknown 
-        List.append [ tva ] (parseTVA' parts (remaining - 1))
+        let vatParts = parts.[parts.Length - remaining].Split('=')
+        let vatRate =
+            match vatParts.[0] with
+            | "VAT0" | "TVA0" -> 0m
+            | "VAT5.5" | "TVA5.5" -> 5.5m
+            | "VAT10" | "TVA10" -> 10m
+            | "VAT20" | "TVA20" -> 20m
+            | unknown -> failwithf "Unknown VAT %s" unknown
+        List.append [ vat vatRate vatParts totalAmount nbVat ] (parseVAT' parts (remaining - 1) totalAmount nbVat)
     else
         []
 
-let parseTVA (parts:array<string>) =
-    let nbTVA = parts.Length - 3
-    parseTVA' parts nbTVA
+let parseVAT (parts:array<string>) totalAmount =
+    let nbVAT = parts.Length - 3
+    parseVAT' parts nbVAT totalAmount nbVAT
 
 let parseReceiptFileName file =
     let fileInfo = new FileInfo(file)
+    printfn "Generating line for %s" fileInfo.Name
     let parts = fileInfo.Name.Replace(fileInfo.Extension, "").Split('-')
+    let totalAmount = parseAmount parts
+    let vat = parseVAT parts totalAmount
+    if vat |> Seq.fold (fun x y -> x + y.Amount + y.AmountExcludingTaxes) 0m <> totalAmount then
+        failwithf "Please give excluding taxes amount, it cannot be inferred..."
     {
         Day = parseDay parts
         Description = parseDescription parts;
-        Amount = parseAmount parts;
-        TVA = parseTVA parts
+        Amount = totalAmount;
+        VAT = vat
         ReceiptLocation = file
     }
 
@@ -63,14 +100,15 @@ let writeCsv directory =
         getReceipts directory
         |> Seq.sortBy (fun x -> x.Day)
         |> Seq.collect (fun x -> 
-            x.TVA
-            |> Seq.map (fun tva -> sprintf "%s;%s;%s;%s;%s;%s" 
+            x.VAT
+            |> Seq.map (fun vat -> 
+                                sprintf "%s;%s;%s;%s;%s;%s" 
                                     (x.Day.ToString("dd/MM/yyyy")) 
                                     x.Description
-                                    (tva.Percent / 100m |> formatDecimal)
-                                    (x.Amount |> formatDecimal)
-                                    (x.Amount - tva.Amount |> formatDecimal)
-                                    (tva.Amount |> formatDecimal)))
+                                    (vat.Rate / 100m |> formatDecimal)
+                                    (vat.AmountExcludingTaxes + vat.Amount |> formatDecimal)
+                                    (vat.AmountExcludingTaxes |> formatDecimal)
+                                    (vat.Amount |> formatDecimal)))
 
     File.WriteAllLines(Path.Combine(directory, "expenseReport.csv"), lines, System.Text.Encoding.UTF8)
 
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ To track my professional expenses, I take a picture of each expense receipt with
 
 Then, since I need to fill in an expense reports regularly, I thought I could encode expense information in picture name, to be able to generate the expense report. That's what the script does with files named as follow : `yyyyMMdd-[expense description]-[amount]-[list of VAT separated by "-"]`, where
 
-- each VAT is formatted : `VAT[%]=[VAT amount]`, can use TVA (in French) instead of VAT, % is restricted to given VAT rates (French ones configured in the script)
+- each VAT is formatted : `VAT[%]=[VAT amount]` or `VAT[%]=[amount excluding VAT]+[VAT amount]`, can use "TVA" (in French) instead of "VAT", % is restricted to given VAT rates (French ones configured in the script). The script will try to infer amount excluding VAT, but in some cases, you must use the second form specifying "amount excluding VAT". 
 - amount (and VAT amount) uses "." for decimal, can add symbol (like e) for the currency (but don't do any conversion for now)
 
 So to generate the expense report, just give the folder containing files to process, it will generate a CSV file with the following columns: `Date (dd/MM/yyyy);Description;VAT rate;amount VAT included;amount without VAT;VAT amount`.
